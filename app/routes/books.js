3fccca3c10099c29c1f18ed31eb3a77cb455ef10
Chequemeout <jackexyoU@gmail.com>
2016-04-17 10:57:50 -0400

    Add modal feature for books

--- app/routes/books.js
+++ app/routes/books.js
@@ -1,9 +1,29 @@
 // app/routes/books.js
 
-import Ember from 'ember';
-
 export default Ember.Route.extend({
+
   model() {
     return this.store.findAll('book');
+  },
+
+  actions: {
+    openCheckoutModal(book) {
+      this.controllerFor('application').set('showingModal', true);
+      return this.render('modal', {
+        outlet: 'modal',
+        into: 'application',
+        model: book,
+        controller: 'application'
+      });
+    },
+
+    closeCheckoutModal() {
+      this.controllerFor('application').set('showingModal', false);
+      return this.disconnectOutlet({
+        outlet: 'modal',
+        parentView: 'application'
+      });
+    }
   }
+
 });
